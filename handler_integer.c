db6be075d1979ff82f0d06c1f7d9927ae2c31e43
eyob721 <eyobmelkamu721@gmail.com>
2023-07-25 15:38:25 +0300

    Add '+' flag feature for signed integer conversion

--- handler_integer.c
+++ handler_integer.c
@@ -12,11 +12,12 @@
  */
 int handle_integer(va_list args, fmt_opts_t *f, char *buf, int *ctr)
 {
-	int printed_chars = 0;
+	int printed_chars = 0, num = va_arg(args, int);
 	char *int_str, int_buf[INT_BUF_SIZE] = "000000000000";
 
-	(void)f;
-	int_str = convert_int_to_str(va_arg(args, int), int_buf, INT_BUF_SIZE);
+	int_str = convert_int_to_str(num, int_buf, INT_BUF_SIZE);
+	if (f->plus_flag == 1 && num >= 0)
+		printed_chars += _putchar_buf('+', buf, ctr);
 	printed_chars += _puts_buf(int_str, buf, ctr);
 	return (printed_chars);
 }
