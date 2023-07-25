9fc7712be6c03052a348cdb841912a8d41430d3e
eyob721 <eyobmelkamu721@gmail.com>
2023-07-25 16:43:55 +0300

    Write test case for hash flag

---
+++ tests/7-main-hash.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  a test program for task 7 ('#' flag)
+ *
+ * Return: 0 on success, or 1 on error
+ */
+int main(void)
+{
+	int sum = 0, i = 0;
+
+	(void)sum;
+	(void)i;
+	_printf("----------------------------------------------------------------\n");
+	_printf("                             OCTAL 							 \n");
+	_printf("----------------------------------------------------------------\n");
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[0]: %#o\n", 0);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[0]: %#o\n", 0);
+	printf("[Exp]: %d\n", sum);
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[7284]: %#o\n", 7284);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[7284]: %#o\n", 7284);
+	printf("[Exp]: %d\n", sum);
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[-7284]: %#o\n", -7284);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[-7284]: %#o\n", -7284);
+	printf("[Exp]: %d\n", sum);
+
+	_printf("----------------------------------------------------------------\n");
+	_printf("                           HEXDECIMAL 							 \n");
+	_printf("----------------------------------------------------------------\n");
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[0]: %#x\n", 0);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[0]: %#x\n", 0);
+	printf("[Exp]: %d\n", sum);
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[7284]: %#x\n", 7284);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[7284]: %#x\n", 7284);
+	printf("[Exp]: %d\n", sum);
+
+	printf("\nTest-%d:\n-----------------------------------------------\n", i++);
+	sum = _printf("[-7284]: %#x\n", -7284);
+	printf("[Got]: %d\n", sum);
+	sum = printf("[-7284]: %#x\n", -7284);
+	printf("[Exp]: %d\n", sum);
+
+	return (0);
+}
